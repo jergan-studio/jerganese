fb4ec1a9f8384bf0253ab70ba756367a00ec48eb
jergan-studio <jose343lopez22@gmail.com>
2026-02-14 17:48:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -27,36 +27,22 @@ io.on("connection", (socket) => {
     };
 
     socket.emit("currentPlayers", players);
-    socket.broadcast.emit("newPlayer", {
-      id: socket.id,
-      player: players[socket.id]
-    });
-
+    socket.broadcast.emit("newPlayer", { id: socket.id, player: players[socket.id] });
     io.emit("playerCount", Object.keys(players).length);
   });
 
   socket.on("move", (data) => {
     if (!players[socket.id]) return;
-
     players[socket.id].x = data.x;
     players[socket.id].y = data.y;
-
-    io.emit("playerMoved", {
-      id: socket.id,
-      player: players[socket.id]
-    });
+    io.emit("playerMoved", { id: socket.id, player: players[socket.id] });
   });
 
   socket.on("chat", (msg) => {
     if (!players[socket.id]) return;
-
     players[socket.id].bubble = msg;
     players[socket.id].bubbleTimer = 180;
-
-    io.emit("chat", {
-      name: players[socket.id].name,
-      message: msg
-    });
+    io.emit("chat", { name: players[socket.id].name, message: msg });
   });
 
   socket.on("disconnect", () => {
@@ -64,10 +50,8 @@ io.on("connection", (socket) => {
     io.emit("playerDisconnected", socket.id);
     io.emit("playerCount", Object.keys(players).length);
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+server.listen(PORT, () => console.log("Server running on port", PORT));
