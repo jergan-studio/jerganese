24c00469583f5ec111f2760a468fedbfc3c1ed4a
jergan-studio <jose343lopez22@gmail.com>
2026-02-14 17:14:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,40 +11,54 @@ app.use(express.static("public"));
 let players = {};
 
 io.on("connection", (socket) => {
-  console.log("Player joined:", socket.id);
 
-  players[socket.id] = {
-    x: 300,
-    y: 300
-  };
+  socket.on("setName", (name) => {
 
-  socket.emit("currentPlayers", players);
-  socket.broadcast.emit("newPlayer", {
-    id: socket.id,
-    player: players[socket.id]
+    players[socket.id] = {
+      x: 300,
+      y: 300,
+      name: name
+    };
+
+    socket.emit("currentPlayers", players);
+    socket.broadcast.emit("newPlayer", {
+      id: socket.id,
+      player: players[socket.id]
+    });
+
+    io.emit("playerCount", Object.keys(players).length);
   });
 
   socket.on("move", (data) => {
-    players[socket.id] = data;
-    io.emit("playerMoved", {
-      id: socket.id,
-      player: data
-    });
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+
+      io.emit("playerMoved", {
+        id: socket.id,
+        player: players[socket.id]
+      });
+    }
   });
 
   socket.on("chat", (msg) => {
-    io.emit("chat", {
-      id: socket.id,
-      message: msg
-    });
+    if (players[socket.id]) {
+      io.emit("chat", {
+        name: players[socket.id].name,
+        message: msg
+      });
+    }
   });
 
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("playerDisconnected", socket.id);
+    io.emit("playerCount", Object.keys(players).length);
   });
 });
 
-server.listen(3000, () => {
-  console.log("Jerganese running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
