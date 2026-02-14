abd95f62c989ffa625c1163813984eba33823751
jergan-studio <jose343lopez22@gmail.com>
2026-02-14 17:29:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ io.on("connection", (socket) => {
     players[socket.id] = {
       x: 300,
       y: 300,
-      name: name
+      name: name,
+      bubble: "",
+      bubbleTimer: 0
     };
 
     socket.emit("currentPlayers", players);
@@ -49,22 +51,4 @@ io.on("connection", (socket) => {
   socket.on("chat", (msg) => {
     if (!players[socket.id]) return;
 
-    io.emit("chat", {
-      name: players[socket.id].name,
-      message: msg
-    });
-  });
-
-  socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("playerDisconnected", socket.id);
-    io.emit("playerCount", Object.keys(players).length);
-  });
-
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+    players[so]()
